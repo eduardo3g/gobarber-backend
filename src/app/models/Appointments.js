d97a6e8c69dda3b0d629c9e0f32fb269c2d2f3c5
= <=>
2019-10-21 09:35:51 -0300

    chore: Implementation of service appointments feature

---
+++ src/app/models/Appointments.js
@@ -0,0 +1,24 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Appointment extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
+
+  static associate(models) {
+    // when a table has two relationships with the same table, it is mandatory to set a alias to the FK (e.g: => as: 'user')
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
+  }
+}
+
+export default Appointment;
---
+++ src/database/migrations/20191021120338-create-appointments.js
@@ -0,0 +1,45 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('appointments', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      date: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' }, // foreign key
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL', // when a user is deleted, keep the apoointments with the column user_id = null
+        allowNull: true,
+      },
+      provider_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' }, // foreign key
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL', // when a user is deleted, keep the apoointments with the column user_id = null
+        allowNull: true,
+      },
+      cancelede_at: {
+        type: Sequelize.DATE,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
+  },
+};
